8b7fa5fa6d64cb0d04cb01ce8942efa696f37e4a
Vorobchuk <vorobchuk@ucu.edu.ua>
2019-12-21 19:54:09 +0200

    Initial commit

--- src/test/java/ua/edu/ucu/StreamAppTest.java
+++ src/test/java/ua/edu/ucu/StreamAppTest.java
@@ -42,5 +42,72 @@ public class StreamAppTest {
         String result = StreamApp.streamForEach(intStream);
         assertEquals(expResult, result);
     }
+    @Test
+    public void testStreamAverage() {
+        System.out.println("StreamAverage");
+        double expResult = 1.0;
+        double result = intStream.average();
+        assertEquals(expResult, (Object) result);
+    }
 
+    @Test
+    public void testStreamMax() {
+        System.out.println("StreamMax");
+        double expResult = 3;
+        double result = intStream.max();
+        assertEquals(expResult, (Object) result);
+    }
+
+    @Test
+    public void testStreamMin() {
+        System.out.println("StreamMin");
+        double expResult = -1;
+        double result = intStream.min();
+        assertEquals(expResult, (Object) result);
+    }
+
+    @Test
+    public void testStreamCount() {
+        System.out.println("StreamCount");
+        double expResult = 5;
+        double result = intStream.count();
+        assertEquals(expResult, (Object) result);
+    }
+
+    @Test
+    public void testStreamSum() {
+        System.out.println("StreamSum");
+        double expResult = 5;
+        double result = intStream.sum();
+        assertEquals(expResult, (Object) result);
+    }
+    @Test
+    public void testStreamFilter() {
+        System.out.println("StreamFilter");
+        int[] expResult = {2, 3};
+        intStream = intStream.filter(x -> x > 1);
+        assertArrayEquals(expResult, intStream.toArray());
+    }
+    @Test
+    public void testStreamMap() {
+        System.out.println("StreamMap");
+        int[] expResult = {1, 0, 1, 4, 9};
+        intStream = intStream.map(x -> x * x);
+        assertArrayEquals(expResult, intStream.toArray());
+    }
+    @Test
+    public void testStreamReduce() {
+        System.out.println("StreamReduce");
+        int expResult = 5;
+        int result = intStream.reduce(0, (sum, x) -> sum += x);
+        assertEquals(expResult, result);
+    }
+    @Test
+    public void testStreamFlatMap() {
+        System.out.println("testStreamFlatMap");
+        int[] expResult = {-3, -1, -2, 0, -1, 1, 0, 2, 1, 3};
+        intStream = intStream.flatMap(x -> AsIntStream.of(x-2, x));
+        assertArrayEquals(expResult, intStream.toArray());
+    }
 }
+
